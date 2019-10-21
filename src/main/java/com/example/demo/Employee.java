adc776ad278ba39176674ff4ba5c69c8f8c7c3b3
sksanthu <56837946+sksanthu@users.noreply.github.com>
2019-10-21 22:34:21 +0530

    Java task what i done till now

---
+++ src/main/java/com/example/demo/Employee.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+public class Employee {
+
+	public int empid;
+	public String empname;
+	public String empskills;
+	public int getEmpid() {
+		return empid;
+	}
+	public void setEmpid(int empid) {
+		this.empid = empid;
+	}
+	public String getEmpname() {
+		return empname;
+	}
+	public void setEmpname(String empname) {
+		this.empname = empname;
+	}
+	public String getEmpskills() {
+		return empskills;
+	}
+	public void setEmpskills(String empskills) {
+		this.empskills = empskills;
+	}
+	
+	
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into employee values(1, 'ramaraju', 'java');
+insert into employee values(2, 'raj', 'servicenow');
+insert into employee values(3, 'santhosh', 'servicenow');
+insert into employee values(4, 'daram', 'servicenow');
